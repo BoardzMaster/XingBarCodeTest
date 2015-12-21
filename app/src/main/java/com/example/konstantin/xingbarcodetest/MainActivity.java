931ccaafdeab43eb5c9dc1ddbb3f2d40f8b68687
Konstantin <kostyameskhidze@gmail.com>
2015-12-21 19:37:29 +0300

    Test version of the barcode app

---
+++ app/src/main/java/com/example/konstantin/xingbarcodetest/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.konstantin.xingbarcodetest;
+
+/**
+ * Created by Konstantin on 21.12.2015.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+public class MainActivity extends ActionBarActivity{
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void launchSimpleActivity(View v) {
+        Intent intent = new Intent(this, SimpleScannerActivity.class);
+        startActivity(intent);
+    }
+
+}
